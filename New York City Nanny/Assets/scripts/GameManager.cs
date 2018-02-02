381fdb3edfaf664630aea3526edd6601470b6415
K8 <kah703@nyu.edu>
2018-02-02 12:29:56 -0500

    Made new scenes

---
+++ New York City Nanny/Assets/scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+        //Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            // Scene Management
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Camera.main.transform.forward);
+            if (hit)
+            {
+                if (hit.collider.gameObject.tag == "bedroom")
+                {
+                    Debug.Log("bedroom");
+                    SceneManager.LoadScene("Bedroom");
+                }
+
+                if (hit.collider.gameObject.tag == "Kitchen")
+                {
+                    Debug.Log("Kitchen");
+                    SceneManager.LoadScene("Kitchen");
+                }
+
+                if (hit.collider.gameObject.tag == "Playroom")
+                {
+                    Debug.Log("Playroom");
+                    SceneManager.LoadScene("Playroom");
+                }
+            }
+
+        }
+        }
+    }
